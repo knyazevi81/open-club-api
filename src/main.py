2ef41f2cfc5bbf7c528b43228c37cad14e2117ce
khasan <khasanknyazev81@gmail.com>
2024-04-28 00:21:37 +0300

    add middleware

--- src/main.py
+++ src/main.py
@@ -17,13 +17,29 @@ app.include_router(main_menue)
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse, PlainTextResponse
 from fastapi.staticfiles import StaticFiles
-from .routes import home
+from fastapi.staticfiles import StaticFiles
+
+from fastapi.middleware.cors import CORSMiddleware
+
+from routes.home import route as main_menue
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="src/routes/templates/static"), name="static")
+origins = [
+    "http://127.0.0.1:5500"
+]
 
-app.include_router(home.route)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount('/static', StaticFiles(directory="routes/templates/static"), name="static")
+
+app.include_router(main_menue)
 
 #if __name__ == '__main__':
 #    print(f"API running {HOST}:{PORT}")
